378c92aec4fb2b9f5c6454d35a8b736e6199671a
Matyas Darabos <m.darabos@students.htl-leonding.ac.at>
2025-12-29 18:00:11 +0100

    Progress update 11 cppTestProj

--- cppTestProject/W3SchoolTutorial/STL/dataStructures.cpp
+++ cppTestProject/W3SchoolTutorial/STL/dataStructures.cpp
@@ -8,6 +8,8 @@
 #include <set>
 #include <map>
 
+#include <algorithm>
+
 using namespace std;
 
 int main(){
@@ -145,18 +147,188 @@ int main(){
 
     deque<string> sharkTypesDeque = {"Great White", "Lemon", "Salmon"};
 
+    cout << "Deque front: " << sharkTypesDeque.front() << endl;
+    cout << "Deque back: " << sharkTypesDeque.back() << endl;
+
     sharkTypesDeque.push_back("Whale");
     sharkTypesDeque.push_front("Hammerhead");
 
+    cout << "Deque front: " << sharkTypesDeque.front() << endl;
+    cout << "Deque back: " << sharkTypesDeque.back() << endl;
+
     sharkTypesDeque.pop_back();
     sharkTypesDeque.pop_front();
+    
+    cout << "Deque front: " << sharkTypesDeque.front() << endl;
+    cout << "Deque back: " << sharkTypesDeque.back() << endl;
+
+    cout << "Deque at 1: " << sharkTypesDeque.at(1) << endl;
 
     sharkTypesDeque.at(1) = "Greenland";
+
+    cout << "Deque at 1: " << sharkTypesDeque.at(1) << endl;
 
     // also has .size() and .empty()
 
 
     
+    cout << "\n\nSets:\n";
+
+    // sets are more or less the same as in other languages
+    // they can't have duplicate elements and they don't have indexes
+
+    set<string> sharkTypesSet = {"Lemon", "Salmon", "Zebra", "Great White", "Lemon"}; // the duplicate Lemon will be removed
+
+    // the values can not be changed, only added or removed
+    // the elements get sorted automatically in ascending order
+    for(string shark : sharkTypesSet){
+        cout << shark << endl;
+    }
+
+    // elements can be added and removed with .insert() and .erase()
+
+    sharkTypesSet.insert("Blacktip");
+    sharkTypesSet.erase("Salmon");
+
+    cout << endl;
+
+    for(string shark : sharkTypesSet){
+        cout << shark << endl;
+    }
+
+    cout << endl;
+
+    // the set can also be stored in descending order
+    set<string, greater<string>> sharkTypesSetDesc = {"Lemon", "Salmon", "Zebra", "Great White"};
+    for(string shark : sharkTypesSetDesc){
+        cout << shark << endl;
+    }
+    
+    //Sets also have the .clear(), .size() and .empty() functions.
+
+
+
+
+    cout << "\n\nSets:\n";
+
+    // Just like in other languages maps elements consist of a value and key
+    // key types are declared first and value types second
+    // all keys have to be unique
+    map<string, string> sharkTypesMap = {{"gw", "Great White"}, {"s", "Salmon"}, {"l", "Lemon"}};
+
+    // the keys can be accessed just like indexes can
+
+    cout << "gw: " << sharkTypesMap["gw"] << endl;
+    cout << "s: " << sharkTypesMap.at("s") << endl;
+
+    // values can also be changed
+    sharkTypesMap.at("s") = "Sleeper";
+
+    cout << "s: " << sharkTypesMap.at("s") << endl;
+
+    // Elements can be added like this:
+    sharkTypesMap["z"] = "Zebra";
+
+    // and like this:
+    sharkTypesMap.insert({"gl", "Greenland"});
+
+    // the key difference between .insert() and just using [] is 
+    // that if the value already exists then the brackets [] overwrite it
+    // but .insert() ignores the new value and keeps the old one
+
+    sharkTypesMap["s"] = "Salmon";
+    cout << "s: " << sharkTypesMap.at("s") << endl;
+
+    sharkTypesMap.insert({"s", "Sleeper"});
+    cout << "s: " << sharkTypesMap.at("s") << endl;
+
+    sharkTypesMap.erase("l"); // removes "Lemon"
+
+    // .clear(), .size() and .empty() also work
+
+    // it can also be checked if a specific element exists with the .count(key) function where key is the key
+    cout << "Does \"l\" exist: " << sharkTypesMap.count("l") << endl;
+
+    // surprisingly enough, maps can be looped trough with a for-each loop
+    // Either use the auto keyword or the pair keyword
+    cout << "Shark map Loop:\n"; 
+    for(pair shark : sharkTypesMap){
+        cout << shark.first << " is: " << shark.second << endl;
+    }
+
+
+
+    // Iterators:
+    // Iterators (just like it's name implies) iterates trough elements of data structures
+    // NOTE: They don't work on stacks and queues
+
+    vector<string>::iterator it;
+
+    cout << "\n\nIterator:\n";
+
+    // iterators are mainly used for manipulating elements in data structures
+    // they are also very usefull for code reusability
+    for(it = sharkTypes.begin(); it != sharkTypes.end(); ++it){
+        cout << *it << endl;
+    }
+
+    // the .begin() and .end() are basically indexes
+    // by doing .begin() + number or .end() - number, where number is an int, a specific elements can be selected
+    // NOTE: .end() points to after the last element
+
+
+    cout << ".end(): " << *sharkTypes.end(); //points to "Tiger" because the first index of sharkTypesList is "Tiger"
+    // and the first element of the sharkTypesList is located after the last sharkTypes element in the memory
+
+    
+    cout << "\n\nReverse iterator:\n";
+
+    // the iterator can also go in reverse order
+    // in this case the iterator has to be a reverse_iterator type
+    for(reverse_iterator it = sharkTypes.rbegin() + 2; it != sharkTypes.rend(); ++it){
+        cout << *it << endl;
+    }
+
+    vector<int> isUneven = {1, 2, 3, 4, 5};
+    
+    vector<int>::iterator isUnevenIterator;
+
+    for(isUnevenIterator = isUneven.begin(); isUnevenIterator != isUneven.end(); ++isUnevenIterator){
+        if(*isUnevenIterator % 2 == 0){
+            *isUnevenIterator = 0;
+        }
+        else{
+            *isUnevenIterator = 1;
+        }
+
+        cout << *isUnevenIterator << endl;
+    }
+
+    // NOTE: remember that maps give the pair type which consists of the key and the variable
+
+    // Algorithms:
+    // Algorithms need to be included trough the <algorithm> library
+
+    cout << "\nSharks sorted:\n";
+
+    sort(sharkTypes.begin(), sharkTypes.end());
+
+    for(string shark : sharkTypes){
+        cout << shark << endl;
+    }
+
+    cout << "\nSharks sorted in reverse order:\n";
+
+    sort(sharkTypes.rbegin(), sharkTypes.rend());
+
+    for(string shark : sharkTypes){
+        cout << shark << endl;
+    }
+
+
+
+    
+    // continue at C++ Algorithms chapter
 
     return 0;
 }
